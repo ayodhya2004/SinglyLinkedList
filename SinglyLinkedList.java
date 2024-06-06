47a8f9db8cb033f8d2dba39bf0aaec2ec7334646
ayodhya2004 <ashiniayodhya3@gmail.com>
2024-06-06 21:27:22 +0530

    Add files via upload

---
+++ SinglyLinkedList.java
@@ -0,0 +1,85 @@
+package linkedlist;
+
+
+public class SinglyLinkedList {
+    private Node head;
+
+    private static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+  
+    public SinglyLinkedList() {
+        head = null;
+    }
+
+  
+    public void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+   
+    public void printList() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public void delete(int data) {
+        if (head == null) {
+            System.out.println("List is empty.");
+            return;
+        }
+
+        if (head.data == data) {
+            head = head.next;
+            return;
+        }
+
+        Node current = head;
+        Node previous = null;
+        while (current != null && current.data != data) {
+            previous = current;
+            current = current.next;
+        }
+
+        if (current == null) {
+            System.out.println("Value not found in the list.");
+            return;
+        }
+
+        previous.next = current.next;
+    }
+
+    public static void main(String[] args) {
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+
+        System.out.print("Linked List: ");
+        list.printList();
+
+        list.delete(20);
+        System.out.print("After deletion: ");
+        list.printList();
+    }
+}
